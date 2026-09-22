749f00e3fff64abcc63bfd6f18e9cc62e15c7491
Greg Benz <git-noreply@gregbenzphotography.com>
2026-09-22 00:02:25 -0500

    Avoid shadowed variable in RGB LUT indexing

--- lib/src/gainmapmath.cpp
+++ lib/src/gainmapmath.cpp
@@ -135,8 +135,8 @@ Color srgbInvOetfLUT(Color e_gamma) {
   static LookUpTable kSrgbLut(kSrgbInvOETFNumEntries, static_cast<float (*)(float)>(srgbInvOetfLUT));
   const std::vector<float>& lut = kSrgbLut.getTable();
   const auto index = [](float value) {
-    int32_t index = static_cast<int32_t>(value * (kSrgbInvOETFNumEntries - 1) + 0.5);
-    return CLIP3(index, 0, kSrgbInvOETFNumEntries - 1);
+    int32_t lut_index = static_cast<int32_t>(value * (kSrgbInvOETFNumEntries - 1) + 0.5);
+    return CLIP3(lut_index, 0, kSrgbInvOETFNumEntries - 1);
   };
   return {{{lut[index(e_gamma.r)], lut[index(e_gamma.g)], lut[index(e_gamma.b)]}}};
 }
